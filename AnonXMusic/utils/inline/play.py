43a0057bc4172be5b3c8693fee1f6cd774b2b2c4
ㅤ𝛎 𝜄 ᥉ ᥴ᥆ ᥉ 𝜄 𝗍 𝛄 ✞ㅤ <166417113+viscity@users.noreply.github.com>
2026-09-25 13:45:48 +0530

    Replace play progress indicator with new character

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -35,27 +35,27 @@ async def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "𐙚—————————"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "—𐙚————————"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "——𐙚———————"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "———𐙚——————"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "————𐙚—————"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "—————𐙚————"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "——————𐙚———"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "———————𐙚——"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "————————𐙚—"
     else:
-        bar = "—————————◉"
-    autoplay_state = await is_autoplay(chat_id)
+        bar = "—————————𐙚"
     buttons = [
+        
         [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
